e883bc8b64188247258503c9ebf557e2c9e0b50a
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-04 11:44:13 +0300

    Project added

---
+++ todo.js
@@ -0,0 +1,52 @@
+// Tüm elementleri seçme işlemi 
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListeners();
+
+function eventListeners() { // All eventListeners
+
+    form.addEventListener("submit", addTodo);
+
+
+}
+
+function addTodo(e) {
+
+    const newTodo = todoInput.value.trim(); //todoInput degerini alır ve bosluklari temizler.
+
+    addTodoToUI(newTodo);
+
+
+
+    e.preventDefault();
+}
+
+function addTodoToUI(newTodo) { // String degerini listItem olarak UI'ya ekler.
+
+    //List Item Olusturma
+    const listItem = document.createElement("li");
+    // Link Olusturma
+    const link = document.createElement("a");
+    link.href = "#";
+    link.className = "delete-item";
+    link.innerHTML = "<i class = 'fa fa-remove'></i>";
+
+    listItem.className = "list-group-item d-flex justify-content-between";
+
+    // Text Node Ekleme
+
+    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(link);
+
+    // Todo List'e List Item Ekleme
+
+    todoList.appendChild(listItem);
+
+    todoInput.value = "";
+}
